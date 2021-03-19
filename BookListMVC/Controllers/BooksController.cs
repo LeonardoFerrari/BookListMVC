d724ff7f85b276c0e41a5a80c66970d32efdf734
LeonardoFerrari <l201164@dac.unicamp.br>
2021-03-19 17:05:28 -0300

    Index view booklist

--- BookListMVC/Controllers/BooksController.cs
+++ BookListMVC/Controllers/BooksController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookListMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,36 @@ namespace BookListMVC.Controllers
 {
     public class BooksController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public BooksController(ApplicationDbContext db) // Dependency Injection
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
             return View();
         }
+
+        #region API Calls
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _db.Books.ToListAsync() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookFromDb = await _db.Books.FirstOrDefaultAsync(u => u.Id == id);
+            if (bookFromDb == null)
+            {
+                return Json(new { success = false, message = "Erro durante remoção" });
+            }
+            _db.Books.Remove(bookFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Item excluido com sucesso!" });
+        }
+        #endregion
     }
 }
---
+++ BookListMVC/wwwroot/js/bookList.js
@@ -0,0 +1,65 @@
+var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#DT_load').DataTable({
+        "ajax": {
+            "url": "/books/getall",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "name", "width": "20%" },
+            { "data": "author", "width": "20%" },
+            { "data": "isbn", "width": "20%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center">
+                        <a href="/Books/Upsert?id=${data}" class='btn btn-success text-white' style='cursor:pointer; width:70px;'>
+                            Editar
+                        </a>
+                        &nbsp;
+                        <a class='btn btn-danger text-white' style='cursor:pointer; width:70px;'
+                            onclick=Delete('/books/Delete?id='+${data})>
+                            Excluir
+                        </a>
+                        </div>`;
+                }, "width": "40%"
+            }
+        ],
+        "language": {
+            "emptyTable": "Nenhum livro encontrado"
+        },
+        "width": "100%"
+    });
+}
+
+
+function Delete(url) {
+    swal.fire({
+        title: "Você tem certeza que deseja excluir esse livro?",
+        text: "Uma vez deletado, o item não poderá ser recuperado",
+        icon: "warning",
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) { // Ou seja, se o usuário realmente desejar excluir o item
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(message);
+                    }
+                }
+            });
+        }
+    });
+}
