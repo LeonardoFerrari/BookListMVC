d3cc0ccdfc211e0aec3955407aec3f07bd2df48b
LeonardoFerrari <l201164@dac.unicamp.br>
2021-03-19 16:30:27 -0300

    Criando Book Model e adicionando no sql

---
+++ BookListMVC/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListMVC.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) // Passa para a classe base
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
